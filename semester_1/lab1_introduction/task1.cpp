e466fbd980c10facb3b76dfbadc11edf80675bf9
Roman <maisonik120@gmail.com>
2025-09-10 09:54:34 +0300

    add task1 and task2

---
+++ semester_1/lab1_introduction/task1.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    double a, b, d;
+//    cout << "vvedite a,zatem b,zatem d: ";
+//    cin >> a >> b >> d;
+//    for (int k = a; k <= b; k = k+d) {
+//        if (k % 3 == 0)
+//            cout << k << " ";
+//    }
+//    return 0;
+//}
