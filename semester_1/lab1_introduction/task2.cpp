989fa241e30c7a8fa8ec563f5011696cc9af3f97
naix <maisonik120@gmail.com>
2025-09-17 09:43:57 +0300

    Update task2.cpp
    std added

--- semester_1/lab1_introduction/task2.cpp
+++ semester_1/lab1_introduction/task2.cpp
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
 #include <iostream>
-using namespace std;
 
 int main() {
     double b;
     double d = 0, e = 1;
-    cout << "vvedite b: ";
-    cin >> b ;
+    std::cout << "vvedite b: ";
+    std::cin >> b ;
     for (int k = 1; k <= b; k++ ) {
         if (k % 2 == 0) {
             d = d + k;
@@ -15,10 +13,7 @@ int main() {
             e = e * k;
         }
     }
-    cout << "summa:" << d << endl;
-    cout << "proizvedenie:"<< e ;
+    std::cout << "summa:" << d << endl;
+    std::cout << "proizvedenie:"<< e ;
     return 0;
 }
-=======
-
->>>>>>> 409b9121f6875feb4c78f72bac66cf32687d4ef5
