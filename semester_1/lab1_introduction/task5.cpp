7ec7e4cca0c3631086f930ce8ffbeee16d6fdcaf
naix <maisonik120@gmail.com>
2025-09-17 09:46:24 +0300

    Update task5.cpp
    std added

--- semester_1/lab1_introduction/task5.cpp
+++ semester_1/lab1_introduction/task5.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main() {
 	int n, m,bigger;
-	cout << "enter n,m";
-	cin >> n >> m;
+	std::cout << "enter n,m";
+	std::cin >> n >> m;
 	if (n > m) {
 		bigger = n;
 	}
 	else {
 		bigger = m;
 	}
-	cout << "common divs: ";
+	std::cout << "common divs: ";
 	for (int i = 1; i <= bigger; i++) {
 		if (n % i == 0 && m % i == 0) {
-			cout << i << ",";
+			std::cout << i << ",";
 		}
 	}
 	return(0);
